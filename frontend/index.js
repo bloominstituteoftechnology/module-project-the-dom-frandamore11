d056687a0a8658190868d92996227cf0eae3080e
Francesco Damore <frandamore11@gmail.com>
2024-04-01 22:37:03 -0600

    finished task 1-4

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,7 @@
+// const { nouns } = require("./data")
+
+// const { people } = require("./data")
+
 function moduleProject1() {
   const footer = document.querySelector('footer')
   const currentYear = new Date().getFullYear()
@@ -28,14 +32,60 @@ function moduleProject1() {
   const widget1 = document.querySelector('.quoteoftheday')
   widget1.appendChild(quote)
 
+  const authorDate = document.createElement('div')
+  const {author, date} = randomQuote
+  authorDate.textContent = `${author} in ${date || 'an unknown date'}`
+  widget1.appendChild(authorDate)
+
   // 👉 TASK 3 - Build a "Corporate Speak" widget
   //  ✨ add your code here
+
+  const randomVerb1 = verbs[Math.floor(Math.random() * verbs.length)]
+  const randomVerb2 = verbs[Math.floor(Math.random() * verbs.length)]
+
+  const randomAdverb1 = adverbs[Math.floor(Math.random() * adverbs.length)]
+  const randomAdverb2 = adverbs[Math.floor(Math.random() * adverbs.length)]
+
+  const randomNoun1 = nouns[Math.floor(Math.random() * nouns.length)]
+  const randomNoun2 = nouns[Math.floor(Math.random() * nouns.length)]
+
+  // console.log(randomVerb1, randomVerb2)
+
+  const sentence = `We need to ${randomVerb1} our ${randomNoun1} ${randomAdverb1} in order to ${randomVerb2} our ${randomNoun2} ${randomAdverb2}.`
+  const paragraph = document.createElement('p')
+  paragraph.textContent = sentence
+  document.querySelector('.corporatespeak').appendChild(paragraph)
+
 
   // 👉 TASK 4 - Build a "Countdown" widget
   //  ✨ add your code here
 
+  const countdownWidget = document.querySelector('.countdown')
+  let count = 5
+  const countdown = document.createElement('p')
+  countdown.textContent = `T-minus ${count}...`
+  countdownWidget.appendChild(countdown)
+
+  setInterval(() => {
+    if(count === 1) {
+      countdown.textContent = 'Liftoff! 🚀'
+    } else {
+      countdown.textContent = `T-minus ${--count}...`
+    }
+  }, 1000);
+
   // 👉 TASK 5 - Build a "Friends" widget
   //  ✨ add your code here
+
+  const randomPerson1 = people[Math.floor(Math.random() * people.length)]
+  // const randomId2 = Math.floor(Math.random() * people.length)
+
+  // console.log(randomId1, randomId2)
+  const personParagraph = document.createElement('p')
+  document.querySelector('.friends').appendChild(personParagraph)
+  const year = randomPerson1.dateOfBirth.split('-')[0]
+  personParagraph.textContent = `${randomPerson1.fname} ${randomPerson1.lname} was born in ${year} and is friends with `
+
 
   // 👉 TASK 6 - Make it so user can tab through the widgets
   //  ✨ add your code here
